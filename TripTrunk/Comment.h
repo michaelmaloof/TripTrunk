bf47b6b5e1b2379a0fe97c129f2bcde4702f6fc8
Michael Maloof <michaelmaloof1991@gmail.com>
2015-04-30 22:06:10 -0400

    design

---
+++ TripTrunk/Comment.h
@@ -0,0 +1,13 @@
+//
+//  Comment.h
+//  TripTrunk
+//
+//  Created by Michael Maloof on 4/30/15.
+//  Copyright (c) 2015 Michael Maloof. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Comment : NSObject
+
+@end
---
+++ TripTrunk/Comment.m
@@ -0,0 +1,13 @@
+//
+//  Comment.m
+//  TripTrunk
+//
+//  Created by Michael Maloof on 4/30/15.
+//  Copyright (c) 2015 Michael Maloof. All rights reserved.
+//
+
+#import "Comment.h"
+
+@implementation Comment
+
+@end
