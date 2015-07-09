ac3e0f575612098a34129d6e8db3746d0ccd436b
Matt Schoch <mattschoch@gmail.com>
2015-07-09 14:10:32 -0400

    begin adding progress view

---
+++ TripTrunk/MSFloatingProgressView.h
@@ -0,0 +1,16 @@
+//
+//  MSFloatingProgressView.h
+//  TripTrunk
+//
+//  Created by Matt Schoch on 7/9/15.
+//  Copyright (c) 2015 Michael Maloof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSFloatingProgressView : UIView
+
+- (id)initWithHeight:(CGFloat)height;
+
+
+@end
