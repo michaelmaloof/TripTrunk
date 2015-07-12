80695467f7eda112f18c07f2547a19190ed69b49
Michael Maloof <michaelmaloof1991@gmail.com>
2015-07-11 21:45:13 -0400

    select profile image

--- TripTrunk/ProfileViewController.m
+++ TripTrunk/ProfileViewController.m
@@ -14,7 +14,7 @@
 #import "FindFriendsViewController.h"
 #import "HomeMapViewController.h"
 
-@interface ProfileViewController ()
+@interface ProfileViewController () <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *profilePicImageView;
 
 @end
@@ -95,15 +95,60 @@
     }
 }
 
-/*
-#pragma mark - Navigation
+- (IBAction)profileImageTapped:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = NO;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    picker.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+    picker.navigationController.title = @"Select profile picture.";
+    [picker.navigationController setTitle:@"Select profile picture."];
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+    picker.navigationBar.tintColor = [UIColor whiteColor];
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+    [self presentViewController:picker animated:YES completion:NULL];}
+
+
+-(void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
+    //    Photo *photo = [Photo object];
+    //    photo.image = info[UIImagePickerControllerOriginalImage];
+    //
+    //    // set the reference URL now so we have it for uploading the raw image data
+    //    photo.imageUrl = [NSString stringWithFormat:@"%@", info[UIImagePickerControllerReferenceURL]];
+    //
+    //    // Set all the generic trip info on the Photo object
+    //    PFUser *user = [PFUser currentUser];
+    //    photo.likes = 0;
+    //    photo.trip = self.trip;
+    //    photo.userName = user.username;
+    //    photo.user = user;
+    //    photo.usersWhoHaveLiked = [[NSMutableArray alloc] init];
+    //    photo.tripName = self.trip.name;
+    //    photo.city = self.trip.city;
+    //
+    //
+    //    [self.photos addObject:photo];
+    //
+    //    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+    //    [picker dismissViewControllerAnimated:YES completion:NULL];
+    //    [self.tripCollectionView reloadData];
+    
 }
-*/
+
+
+-(void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
+    [picker dismissViewControllerAnimated:YES completion:NULL];
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleLightContent;
+    
+}
+
+- (void)navigationController:(UINavigationController *)navigationController willShowViewController:(UIViewController *)viewController animated:(BOOL)animated
+{
+    [viewController.navigationItem setTitle:@"Select Profile Image"];
+}
+
+
 
 @end
 
