14f363a0666cdc9ddc4ea7d40f887242f874d2b6
Michael Maloof <michaelmaloof1991@gmail.com>
2015-08-25 21:57:54 -0400

    No Trunks Here
    "No Trunks" doesnt show o ntrunk list unless the count is 0 or the array we get back is nil. So we only show this if there are no trunks and not when we are loading the trunks. This will be visible once a different bug is fixed

--- TripTrunk/TrunkListViewController.m
+++ TripTrunk/TrunkListViewController.m
@@ -86,8 +86,8 @@
     self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
     
     // Setup Empty Datasets
-    self.tableView.emptyDataSetDelegate = self;
-    self.tableView.emptyDataSetSource = self;
+//    self.tableView.emptyDataSetDelegate = self;
+//    self.tableView.emptyDataSetSource = self;
 }
 
 -(void)viewWillAppear:(BOOL)animated {
@@ -111,9 +111,17 @@
             if(error)
             {
                 NSLog(@"Error: %@",error);
+                    self.tableView.emptyDataSetDelegate = self;
+                    self.tableView.emptyDataSetSource = self;
             }
             else
             {
+                
+                if (objects.count == 0 || objects == nil){
+                    self.tableView.emptyDataSetDelegate = self;
+                    self.tableView.emptyDataSetSource = self;
+                }
+                
                 int count = 0;
                 self.meParseLocations = [[NSMutableArray alloc]init];
                 for (PFObject *activity in objects){
@@ -162,10 +170,16 @@
         [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
             if(error)
             {
-                NSLog(@"Error: %@",error);
+                self.tableView.emptyDataSetDelegate = self;
+                self.tableView.emptyDataSetSource = self;
             }
             else
             {
+                if (objects.count == 0 || objects == nil){
+                    self.tableView.emptyDataSetDelegate = self;
+                    self.tableView.emptyDataSetSource = self;
+                }
+                
                 int count = 0;
                 self.meParseLocations = [[NSMutableArray alloc]init];
                 for (PFObject *activity in objects){
