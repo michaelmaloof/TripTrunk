7ba68db2bc06e7b14b0f43e0513bdfaae20c0e8e
Michael Maloof <michaelmaloof1991@gmail.com>
2015-10-10 18:23:56 -0400

    documenting code

--- TripTrunk/TTBaseTableViewController.m
+++ TripTrunk/TTBaseTableViewController.m
@@ -16,14 +16,15 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
-    [super viewDidLoad];
+
+//This is to remove the titles under the tab bar icons
     [[self.tabBarController.viewControllers objectAtIndex:0] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:1] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:2] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:3] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:4] setTitle:@""];
-    
+
+//This is to remove the word "Back" on the nav bar. We want there just to be an arrow @"<".
     UIBarButtonItem *newBackButton =
     [[UIBarButtonItem alloc] initWithTitle:@""
                                      style:UIBarButtonItemStylePlain
@@ -34,6 +35,9 @@
 }
 
 -(void)viewWillAppear:(BOOL)animated{
+    
+//This is to remove the titles under the tab bar icons
+
     [[self.tabBarController.viewControllers objectAtIndex:0] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:1] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:2] setTitle:@""];
--- TripTrunk/TTBaseViewController.m
+++ TripTrunk/TTBaseViewController.m
@@ -16,12 +16,16 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+//This is to remove the titles under the tab bar icons
     [[self.tabBarController.viewControllers objectAtIndex:0] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:1] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:2] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:3] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:4] setTitle:@""];
     
+    
+//This is to remove the word "Back" on the nav bar. We want there just to be an arrow @"<".
     UIBarButtonItem *newBackButton =
     [[UIBarButtonItem alloc] initWithTitle:@""
                                      style:UIBarButtonItemStylePlain
@@ -30,11 +34,7 @@
     [[self navigationItem] setBackBarButtonItem:newBackButton];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
+//This is to remove the titles under the tab bar icons
 -(void)viewWillAppear:(BOOL)animated{
     [[self.tabBarController.viewControllers objectAtIndex:0] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:1] setTitle:@""];
@@ -42,14 +42,6 @@
     [[self.tabBarController.viewControllers objectAtIndex:3] setTitle:@""];
     [[self.tabBarController.viewControllers objectAtIndex:4] setTitle:@""];
 }
-/*
-#pragma mark - Navigation
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
