afaf3910fe0d87a4c5b60405ca67b8d49cfef07b
Michael Maloof <michaelmaloof1991@gmail.com>
2015-10-11 18:40:12 -0400

    TTUtility nslocalized string

--- TripTrunk/SocialUtility.m
+++ TripTrunk/SocialUtility.m
@@ -123,7 +123,7 @@
     
     dispatch_async(dispatch_get_main_queue(), ^{
         HUD = [MBProgressHUD showHUDAddedTo:[[[UIApplication sharedApplication] delegate] window] animated:YES];
-        HUD.labelText = @"Blocking...";
+        HUD.labelText = NSLocalizedString(@"Blocking...",@"Blocking...");
         HUD.mode = MBProgressHUDModeText; // change to Determinate to show progress
     });
     
@@ -138,7 +138,7 @@
     [block saveEventually];
     dispatch_async(dispatch_get_main_queue(), ^{
         // Hide HUD spinner
-        HUD.labelText = @"Done!";
+        HUD.labelText = NSLocalizedString(@"Done!",@"Done!");
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             [MBProgressHUD hideHUDForView:[[[UIApplication sharedApplication] delegate] window] animated:YES];
         });
