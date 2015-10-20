4607a0d224c1c4c72b647b81476be6c73d677c18
Michael Maloof <michaelmaloof1991@gmail.com>
2015-10-19 23:09:20 -0400

    reset password

---
+++ TripTrunk/TTForgotPasswordViewController.m
@@ -0,0 +1,37 @@
+//
+//  TTForgotPasswordViewController.m
+//  TripTrunk
+//
+//  Created by Michael Maloof on 10/19/15.
+//  Copyright © 2015 Michael Maloof. All rights reserved.
+//
+
+#import "TTForgotPasswordViewController.h"
+
+@interface TTForgotPasswordViewController ()
+
+@end
+
+@implementation TTForgotPasswordViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
