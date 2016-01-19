e6125690ed80780f78203671e9c586f44c34213d
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-01-19 20:29:08 -0200

    Error handling for linking facebook account

--- TripTrunk/EditProfileViewController.m
+++ TripTrunk/EditProfileViewController.m
@@ -217,15 +217,137 @@
 
 - (IBAction)addFacebook:(id)sender
 {
-    
+    //List of permissions we want from the user's facebook to link tp the parse user. We don't need the email since we won't be changing their current email to their facebook email.
     NSArray *permissionsArray = @[@"public_profile", @"user_friends"];
     
+    //Make sure the user isnt already linked with facebook
     if ([PFFacebookUtils isLinkedWithUser:[PFUser currentUser]] == NO)
     {
-        [PFFacebookUtils linkUserInBackground:[PFUser currentUser] withReadPermissions:permissionsArray block:^(BOOL succeeded, NSError * _Nullable error) {
-            
-        }];
+        [PFFacebookUtils linkUserInBackground:[PFUser currentUser] withReadPermissions:permissionsArray block:^(BOOL succeeded, NSError * _Nullable error)
+         {
+             
+             if (error)
+             {
+                 //ERROR HANDLE: User Was Unable to link with facebook please try again or contact austin
+                 UIAlertView *alertView = [[UIAlertView alloc] init];
+                 alertView.delegate = self;
+                 alertView.title = NSLocalizedString(@"Something went wrong",@"Something went wrong");
+                 alertView.message = NSLocalizedString(@"Please try again or contact austinbarnard@triptrunkapp.com.",@"Please try again or contact austinbarnard@triptrunkapp.com.");
+                 alertView.backgroundColor = [UIColor colorWithRed:131.0/255.0 green:226.0/255.0 blue:255.0/255.0 alpha:1.0];
+                 [alertView addButtonWithTitle:NSLocalizedString(@"OK",@"OK")];
+                 [alertView show];
+                 
+             } else //succesfully connected the parse user to their facebook account
+             {
+                 //we need to logout the user and log them back in for the fbid in parse to update. Its annoying and we should see if we can fix it.
+                 [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error)
+                  {
+                      if (error)
+                      {
+                        //ERROR HANDLE: tell the user we linked the acccount succefully but you need to log back in with the login with facebook option for the link to go into effect
+                          [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+                              UIAlertView *alertView = [[UIAlertView alloc] init];
+                              alertView.delegate = self;
+                              alertView.title = NSLocalizedString(@"Your Facebook account was linked!. Please log back in using Facebook to continue.",@"Your Facebook account was linked!. Please log back in using Facebook to continue.");
+                              alertView.backgroundColor = [UIColor colorWithRed:131.0/255.0 green:226.0/255.0 blue:255.0/255.0 alpha:1.0];
+                              [alertView addButtonWithTitle:NSLocalizedString(@"OK",@"OK")];
+                              [alertView show];
+                              
+                              [self.tabBarController setSelectedIndex:0];
+                          }];
+                          
+                      } else {
+                          [self loginWithFacebook];
+                      }
+                      
+                  }];
+             }
+         }];
     }
+}
+
+-(void)loginWithFacebook{
+    // Set permissions required from the facebook user account
+    NSArray *permissionsArray = @[ @"email", @"public_profile", @"user_friends"];
+    
+    // Login PFUser using Facebook
+    [PFFacebookUtils logInInBackgroundWithReadPermissions:permissionsArray block:^(PFUser *user, NSError *error)
+     {
+         
+         if (error)
+         {
+        //ERROR HANDLE: tell the user we linked the acccount but we need them to relogin, then take them to the login screen
+             NSString *errorString = [error userInfo][@"error"];
+             NSLog(@"%@",errorString);
+             
+             [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+                 UIAlertView *alertView = [[UIAlertView alloc] init];
+                 alertView.delegate = self;
+                 alertView.title = NSLocalizedString(@"Your Facebook account was linked!. Please log back in using Facebook to continue.",@"Your Facebook account was linked!. Please log back in using Facebook to continue.");
+                 alertView.backgroundColor = [UIColor colorWithRed:131.0/255.0 green:226.0/255.0 blue:255.0/255.0 alpha:1.0];
+                 [alertView addButtonWithTitle:NSLocalizedString(@"OK",@"OK")];
+                 [alertView show];
+                 
+                 [self.tabBarController setSelectedIndex:0];
+             }];
+
+             
+             return;
+             
+         }
+         
+         if (!user)
+         {
+             [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+                 UIAlertView *alertView = [[UIAlertView alloc] init];
+                 alertView.delegate = self;
+                 alertView.title = NSLocalizedString(@"Your Facebook account was linked!. Please log back in using Facebook to continue.",@"Your Facebook account was linked!. Please log back in using Facebook to continue.");
+                 alertView.backgroundColor = [UIColor colorWithRed:131.0/255.0 green:226.0/255.0 blue:255.0/255.0 alpha:1.0];
+                 [alertView addButtonWithTitle:NSLocalizedString(@"OK",@"OK")];
+                 [alertView show];
+                 
+                 [self.tabBarController setSelectedIndex:0];
+             }];
+
+         } else
+         {
+             
+             if ([user objectForKey:@"fbid"] == nil)
+             {
+                 FBSDKGraphRequest *request = [[FBSDKGraphRequest alloc] initWithGraphPath:@"me" parameters:nil];
+                 [request startWithCompletionHandler:^(FBSDKGraphRequestConnection *connection, id result, NSError *error) {
+                     if (!error)
+                     {
+                         // result is a dictionary with the user's Facebook data
+                         NSDictionary *userData = (NSDictionary *)result;
+                         PFUser *user = [PFUser currentUser];
+                         NSString *fbid = [userData objectForKey:@"id"];
+                         if (fbid)
+                         {
+                             [user setObject:fbid forKey:@"fbid"];
+                             [user saveInBackground];
+                         }
+                     } else {
+                        //ERROR HANDLE: tell the user we linked the acccount but we need them to relogin, then take them to the login screen
+                         
+                         [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
+                             UIAlertView *alertView = [[UIAlertView alloc] init];
+                             alertView.delegate = self;
+                             alertView.title = NSLocalizedString(@"Your Facebook account was linked!. Please log back in using Facebook to continue.",@"Your Facebook account was linked!. Please log back in using Facebook to continue.");
+                             alertView.backgroundColor = [UIColor colorWithRed:131.0/255.0 green:226.0/255.0 blue:255.0/255.0 alpha:1.0];
+                             [alertView addButtonWithTitle:NSLocalizedString(@"OK",@"OK")];
+                             [alertView show];
+                             
+                             [self.tabBarController setSelectedIndex:0];
+                         }];
+
+                     }
+                 }];
+                 
+             }
+         }
+     }];
+    
 }
 
 
--- TripTrunk/UserProfileViewController.m
+++ TripTrunk/UserProfileViewController.m
@@ -1,3 +1,4 @@
+
 //
 //  UserProfileViewController.m
 //  TripTrunk
