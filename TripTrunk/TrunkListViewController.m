16b6bf6895863976011d179c6667a5157b80d91d
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-02-22 12:13:12 -0300

    toggle maintance list

--- TripTrunk/TrunkListViewController.m
+++ TripTrunk/TrunkListViewController.m
@@ -107,6 +107,7 @@
     
     if (self.isList == YES){
         self.navigationController.navigationBar.tintColor = [UIColor whiteColor];
+        self.navigationItem.rightBarButtonItem.enabled = NO;
         [self loadTrunkListBasedOnProfile];
     }
     
@@ -124,10 +125,12 @@
 
         
         self.filter.tag = 0;
+        self.navigationItem.rightBarButtonItem.enabled = NO;
         [self queryParseMethodEveryone];
         
         
     } else {
+        self.navigationItem.rightBarButtonItem.enabled = NO;
         [self loadUserTrunks];
     }
     
@@ -168,8 +171,9 @@
  */
 -(void)loadUserTrunks
 {
-    if (self.meParseLocations.count == 0) {
+    if (self.meParseLocations.count == 0 || self.meParseLocations.class == nil) {
         NSDate *lastOpenedApp = [PFUser currentUser][@"lastUsed"];
+        
         PFQuery *query = [PFQuery queryWithClassName:@"Activity"];
         if (!self.user){
             [query whereKey:@"toUser" equalTo:[PFUser currentUser]];
@@ -184,8 +188,12 @@
         [query whereKeyExists:@"trip"];
         [query includeKey:@"trip.creator"];
         [query includeKey:@"trip.publicTripDetail"];
-//        [query orderByDescending:@"createdAt"]; //TODO does this actually work?
-        query.limit = 50;
+        [query orderByDescending:@"createdAt"]; //TODO does this actually work?
+
+        
+        
+        
+        query.limit = 100;
         query.skip = self.objectsCountMe;
         
         [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
@@ -238,12 +246,15 @@
                 
             }
             //                self.filter.tag = 1;
+            self.navigationItem.rightBarButtonItem.enabled = YES;
             [self.tableView reloadData];
         }];
     } else
     {
+        self.navigationItem.rightBarButtonItem.enabled = YES;
         [self.tableView reloadData];
     }
+
 }
 
 -(void)loadTrunkListBasedOnProfile{
@@ -315,10 +326,12 @@
                 
             }
             //                self.filter.tag = 1;
+            self.navigationItem.rightBarButtonItem.enabled = YES;
             [self.tableView reloadData];
         }];
     } else
     {
+        self.navigationItem.rightBarButtonItem.enabled = YES;
         [self.tableView reloadData];
     }
 }
@@ -330,6 +343,8 @@
  *
  */
 -(void)rightBarItemWasTapped {
+    
+    self.navigationItem.rightBarButtonItem.enabled = NO;
     if (self.filter.tag == 0) {
         [self.filter setImage:[UIImage imageNamed:@"all_mine_2"]];
         self.filter.tag = 1;
@@ -402,7 +417,7 @@
         }];
         
     } else
-    {
+    {    self.navigationItem.rightBarButtonItem.enabled = YES;
         [self.tableView reloadData];
     }
 
@@ -432,6 +447,7 @@
         if(error)
         {
             NSLog(@"Error: %@",error);
+            self.navigationItem.rightBarButtonItem.enabled = YES;
             [self.tableView reloadData];
         }
         {
@@ -478,6 +494,7 @@
             
         }
         self.filter.tag = 0;
+        self.navigationItem.rightBarButtonItem.enabled = YES;
         [self.tableView reloadData];
         
     }];
@@ -656,7 +673,7 @@
 
 - (NSAttributedString *)titleForEmptyDataSet:(UIScrollView *)scrollView
 {
-    NSString *text = NSLocalizedString(@"No One Has Been here :/",@"No One Has Been Here :/");
+    NSString *text = NSLocalizedString(@"No One Has Been Here :/",@"No One Has Been Here :/");
     
     NSDictionary *attributes = @{NSFontAttributeName: [UIFont boldSystemFontOfSize:18.0],
                                  NSForegroundColorAttributeName: [UIColor whiteColor]};
@@ -826,7 +843,7 @@
     if (hasSeen == YES){
         [self.haventSeens removeObject:trunk];
     }
-    
+    self.navigationItem.rightBarButtonItem.enabled = YES;
     [self.tableView reloadData];
 
 }
@@ -849,7 +866,8 @@
     
     [self.parseLocations removeObject:tripA];
     [self.meParseLocations removeObject:tripB];
-    
+    self.navigationItem.rightBarButtonItem.enabled = YES;
+
     [self.tableView reloadData];
 
 }
