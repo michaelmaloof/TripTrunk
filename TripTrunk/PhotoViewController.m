83e3b66fbc5d5d273dc261acc6e47c175626d2ae
Michael Maloof 13' Pro <michaelmaloof1991@gmail.com>
2016-04-14 20:35:48 -0300

    temp comment fix

--- TripTrunk/PhotoViewController.m
+++ TripTrunk/PhotoViewController.m
@@ -1079,8 +1079,9 @@
 -(void)updateCommentsLabel{
     NSString *comments = NSLocalizedString(@"Comments",@"Comments");
     NSNumber *commentCount = [[TTCache sharedCache] commentCountForPhoto:self.photo];
-    if([commentCount intValue] < 2)
+    if([commentCount intValue] < 2){
         comments = NSLocalizedString(@"Comment",@"Comment");
+    }
     
     if (commentCount.integerValue == 0){
         [self.comments setTitle:comments forState:UIControlStateNormal];
