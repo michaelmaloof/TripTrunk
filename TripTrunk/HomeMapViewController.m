7d9177670618faf29a3ced0018c0262e1ff091b1
Michael_Maloof_MacPro_2010 <michaelmaloof1991@gmail.com>
2016-05-17 13:14:45 -0700

    New Unseen Logo

--- TripTrunk/HomeMapViewController.m
+++ TripTrunk/HomeMapViewController.m
@@ -850,7 +850,7 @@
         startAnnotation.alpha = 1.0;
         //        [[startAnnotation superview] bringSubviewToFront:startAnnotation];
         startAnnotation.layer.zPosition = 1;
-        startAnnotation.frame = CGRectMake(startAnnotation.frame.origin.x, startAnnotation.frame.origin.y, startAnnotation.frame.size.width*1.3, startAnnotation.frame.size.height*1.3);
+        startAnnotation.frame = CGRectMake(startAnnotation.frame.origin.x, startAnnotation.frame.origin.y, startAnnotation.frame.size.width*1.0, startAnnotation.frame.size.height*1.0);
         
     } else {
         if (hasSeen == NO){
@@ -863,7 +863,7 @@
         startAnnotation.alpha = 1.0;
         //        [[startAnnotation superview] sendSubviewToBack:startAnnotation];
         startAnnotation.layer.zPosition = .9;
-        startAnnotation.frame = CGRectMake(startAnnotation.frame.origin.x, startAnnotation.frame.origin.y, startAnnotation.frame.size.width*1.1, startAnnotation.frame.size.height*1.1);
+        startAnnotation.frame = CGRectMake(startAnnotation.frame.origin.x, startAnnotation.frame.origin.y, startAnnotation.frame.size.width*.9, startAnnotation.frame.size.height*.9);
         
         
         
