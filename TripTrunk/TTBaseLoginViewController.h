210996d13761f06c0cc1467bc510f93b087d6e32
Michael Maloof <michaelmaloof1991@gmail.com>
2017-01-08 15:25:05 -0500

    TripTrunk - Login RD - Michael Maloof
    Added all the assets we needed for Login, Created a New Login Storyboard, Created the Base Logiv View Controllers.

---
+++ TripTrunk/TTBaseLoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  BaseLoginViewController.h
+//  TripTrunk
+//
+//  Created by Michael Maloof on 1/4/17.
+//  Copyright © 2017 Michael Maloof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "TTBaseViewController.h"
+
+@interface TTBaseLoginViewController : TTBaseViewController
+
+@end
