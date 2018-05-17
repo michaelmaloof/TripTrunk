a0b4a9fabd55259a1795ed49b8f7fb6c3809196d
Archon Light <mike@altri.us>
2018-05-17 15:15:20 -0400

    search filtering in place in Search Tab (still need follow buttons & status)

--- TripTrunk/TTSearchViewController.m
+++ TripTrunk/TTSearchViewController.m
@@ -19,13 +19,16 @@
 #import "TTAnalytics.h"
 #import "TTUsernameSort.h"
 #import "TTFindFriendsViewCell.h"
+#import "TTPopoverProfileViewController.h"
 
-@interface TTSearchViewController () <UserTableViewCellDelegate, UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate, UIAlertViewDelegate>
+@interface TTSearchViewController () <UserTableViewCellDelegate, UISearchControllerDelegate, UISearchBarDelegate, UISearchResultsUpdating, DZNEmptyDataSetSource, DZNEmptyDataSetDelegate, UIAlertViewDelegate,UICollectionViewDelegate,UICollectionViewDataSource,UIPopoverPresentationControllerDelegate,UIGestureRecognizerDelegate>
 
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 @property (strong, nonatomic) IBOutlet TTOnboardingTextField *searchTextField;
 @property (strong, nonatomic) NSMutableArray *promoted;
 @property (strong, nonatomic) UISearchController *searchController;
+@property (strong, nonatomic) UIPopoverPresentationController *popover;
+@property (strong, nonatomic) TTPopoverProfileViewController *popoverProfileViewController;
 @property NSString *searchString;
 @property PFUser *user;
 @property BOOL loadedOnce;
@@ -35,6 +38,7 @@
 @property (strong, nonatomic) NSMutableArray *pending; // users this user has requested to follow
 @property int searchCount;
 @property BOOL removeResults;
+@property BOOL isSearching;
 @property BOOL friendsMaxed;
 @property BOOL isLoadingFollowing;
 @property BOOL isLoadingPending;
@@ -299,7 +303,9 @@
 }
 
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
-    return self.promoted.count;
+    if(self.searchResults)
+        return self.searchResults.count;
+    else return self.promoted.count;
 }
 
 
@@ -315,12 +321,18 @@
     friendCell.profilePic.image = [UIImage imageNamed:@"square_placeholder"];
     friendCell.followButton.userInteractionEnabled = NO;
     [friendCell.followButton setTitle:@"FOLLOW" forState:UIControlStateNormal];
+    friendCell.selectionStyle = UITableViewCellSelectionStyleNone;
     
     PFUser *user;
-    id promotedUser = self.promoted[indexPath.row];
-    if([promotedUser isKindOfClass:[PFUser class]])
-        user = promotedUser;
-    else user = promotedUser[@"user"];
+    if(self.searchResults){
+        user = self.searchResults[indexPath.row];
+    }else{
+        id promotedUser = self.promoted[indexPath.row];
+//        if([promotedUser isKindOfClass:[PFUser class]])
+//            user = promotedUser;
+//        else
+        user = promotedUser[@"user"];
+    }
     
     friendCell.firstLastName.text = [NSString stringWithFormat:@"%@ %@",user[@"firstName"], user[@"lastName"]];
     
@@ -368,8 +380,226 @@
     //    }
 }
 
-- (void)activityCell:(UICollectionViewCell *)cellView didPressUsernameForUser:(PFUser *)user{
+//- (void)activityCell:(UICollectionViewCell *)cellView didPressUsernameForUser:(PFUser *)user{
+//
+//}
+
+- (IBAction)longPressGestureToPreviewProfile:(UILongPressGestureRecognizer *)sender {
+    if(sender.state == UIGestureRecognizerStateBegan){
+        UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Profile" bundle:nil];
+        self.popoverProfileViewController = (TTPopoverProfileViewController *)[storyboard instantiateViewControllerWithIdentifier:@"ProfilePopoverView"];
+        CGPoint touchPoint = [sender locationInView:self.view];
+        UIView* touchedView = [self.view hitTest:touchPoint withEvent:nil];
+        if([touchedView isKindOfClass:[TTUserProfileImage class]]){
+            PFUser *user;
+            if(self.searchResults)
+                user = self.searchResults[touchedView.tag];
+            else user = self.promoted[touchedView.tag][@"user"];
+            
+            self.popoverProfileViewController.user = user;
+            self.popoverProfileViewController.modalPresentationStyle = UIModalPresentationPopover;
+            self.popoverProfileViewController.modalTransitionStyle = UIModalTransitionStyleCrossDissolve;
+            
+            //force the popover to display like an iPad popover otherwise it will be full screen
+            self.popover  = self.popoverProfileViewController.popoverPresentationController;
+            self.popover.delegate = self;
+            self.popover.sourceView = self.view;
+            self.popover.sourceRect = CGRectMake(27,140,320,380);
+            self.popover.permittedArrowDirections = 0;
+            
+            self.popoverProfileViewController.preferredContentSize = CGSizeMake(320,380);
+            self.popoverProfileViewController.popoverPresentationController.sourceView = self.view;
+            self.popoverProfileViewController.popoverPresentationController.sourceRect = CGRectMake(27,140,320,380);
+            
+            //HACK because modalTransitionStyle doesn't work on fade in
+            CATransition* transition = [CATransition animation];
+            transition.duration = 0.5;
+            transition.type = kCATransitionFade;
+            [self.view.window.layer addAnimation:transition forKey:kCATransition];
+            
+            [self presentViewController:self.popoverProfileViewController animated:NO completion:nil];
+        }
+    }
     
+    if(sender.state == UIGestureRecognizerStateEnded){
+        [self.popoverProfileViewController dismissViewControllerAnimated:YES completion:nil];
+    }
+}
+
+#pragma mark - UIModalPopoverDelegate
+- (UIModalPresentationStyle) adaptivePresentationStyleForPresentationController: (UIPresentationController * ) controller {
+    return UIModalPresentationNone;
+}
+
+-(BOOL)amIAlreadyFollowingThisUser:(NSString*)fbid{
+    for(PFUser* user in self.friends){
+        NSString *userFbid = [NSString stringWithFormat:@"%@",user[@"fbid"]];
+        if([userFbid isEqualToString:fbid]){
+            return YES;
+        }
+    }
+    return NO;
+}
+
+//-(void)setFollowStatus:(UIButton *)sender {
+//    if ([sender isSelected] == YES) {
+//        // Unfollow
+//        [sender setSelected:NO]; // change the button for immediate user feedback
+//        //        [sender setTitle:@"Follow" forState:UIControlStateNormal];
+//        //        sender.backgroundColor = [UIColor whiteColor];
+//        //        sender.titleLabel.textColor = [TTColor tripTrunkRed];
+//        //        [sender setTitleColor:[TTColor tripTrunkRed] forState:UIControlStateNormal];
+//        //        [self.currentUserFriends removeObject:user.objectId];
+//        [SocialUtility unfollowUser:self.facebookFriends[sender.tag] block:^(BOOL succeeded, NSError *error) {
+//            if(error){
+//                NSLog(@"Error: %@", error);
+//                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Unfollow Failed"
+//                                                                message:@"Please try again"
+//                                                               delegate:self
+//                                                      cancelButtonTitle:@"Okay"
+//                                                      otherButtonTitles:nil, nil];
+//                [sender setSelected:YES];
+//                [alert show];
+//            }else{
+//                NSLog(@"User unfollowed");
+//            }
+//        }];
+//    }
+//    else {
+//        // Follow
+//        [sender setSelected:YES];
+//        sender.titleLabel.textColor = [UIColor whiteColor];
+//        [sender setTitleColor:[TTColor tripTrunkWhite] forState:UIControlStateNormal];
+//        //        [self.currentUserFriends addObject:user.objectId];
+//        [SocialUtility followUserInBackground:self.facebookFriends[sender.tag] block:^(BOOL succeeded, NSError *error) {
+//            if (error) {
+//                //                sender.titleLabel.textColor = [TTColor tripTrunkRed];
+//                //                [sender setTitleColor:[TTColor tripTrunkRed] forState:UIControlStateNormal];
+//                NSLog(@"Error: %@", error);
+//                //                [self.currentUserFriends removeObject:user.objectId];
+//                NSLog(@"Follow failed");
+//                UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Follow Failed"
+//                                                                message:@"Please try again"
+//                                                               delegate:self
+//                                                      cancelButtonTitle:@"Okay"
+//                                                      otherButtonTitles:nil, nil];
+//                [sender setSelected:NO];
+//                [alert show];
+//            }else{
+//                NSLog(@"User followed");
+//            }
+//        }];
+//    }
+//}
+
+//- (IBAction)tapGestureRecognizerForFollowButton:(UITapGestureRecognizer *)sender {
+//    CGPoint touchPoint = [sender locationInView:self.view];
+//    UIView* touchedView = [self.view hitTest:touchPoint withEvent:nil];
+//    CGPoint touchPointInCell = [sender locationInView:touchedView];
+//    NSArray *views = [touchedView subviews];
+//    for(id view in views){
+//        if([view isKindOfClass:[UIButton class]]){
+//            UIButton *buttonView = (UIButton*)view;
+//            if(CGRectContainsPoint(buttonView.frame, touchPointInCell))
+//                [self setFollowStatus:(UIButton*)view];
+//            break;
+//        }
+//    }
+//}
+
+-(NSString*)getInitialsForMissingProfilePictureFromUser:(PFUser*)user{
+    return [NSString stringWithFormat:@"%@%@",[user[@"firstName"] substringToIndex:1],[user[@"lastName"] substringToIndex:1]];;
+}
+
+#pragma mark - Friend Search
+- (void)filterResults:(NSString *)searchTerm {
+    [NSObject cancelPreviousPerformRequestsWithTarget:self selector:@selector(dismissKeyboard) object:nil];
+    if (![searchTerm isEqualToString:@""]){
+        //[self.searchResults removeAllObjects];
+        
+        
+        //TODO: add NOT IN existingUsers query to both of these
+        
+        //     Gets all the users who have blocked this user. Hopefully it's 0!
+        PFQuery *blockQuery = [PFQuery queryWithClassName:@"Block"];
+        [blockQuery whereKey:@"blockedUser" equalTo:[PFUser currentUser]];
+        
+        PFQuery *usernameQuery = [PFUser query];
+        [usernameQuery whereKeyExists:@"username"];  //this is based on whatever query you are trying to accomplish
+        [usernameQuery whereKey:@"username" containsString:searchTerm];
+        [usernameQuery whereKey:@"username" notEqualTo:[[PFUser currentUser] username]];
+        [usernameQuery whereKeyExists:@"completedRegistration"]; // Make sure we don't get half-registered users with the weird random usernames
+        
+        PFQuery *nameQuery = [PFUser query];
+        [nameQuery whereKeyExists:@"lowercaseName"];  //this is based on whatever query you are trying to accomplish
+        [nameQuery whereKeyExists:@"completedRegistration"];// Make sure we don't get half-registered users with the weird random usernames
+        [nameQuery whereKey:@"lowercaseName" containsString:[searchTerm lowercaseString]];
+        [nameQuery whereKey:@"username" notEqualTo:[[PFUser currentUser] username]]; // exclude currentUser
+        
+        PFQuery *query = [PFQuery orQueryWithSubqueries:@[usernameQuery, nameQuery]];
+        query.limit = 10;
+        //FIXME SEARCH NEEDS A SKIP OR ITLL KEEP RETURNING THE SAME ONES
+        
+        [query findObjectsInBackgroundWithBlock:^(NSArray * _Nullable objects, NSError * _Nullable error) {
+            
+            if (error){
+                [ParseErrorHandlingController handleError:error];
+            } else {
+                self.searchResults = [[NSMutableArray alloc] init];
+                [self.searchResults addObjectsFromArray:objects];
+                TTUsernameSort *us = [[TTUsernameSort alloc] init];
+                NSArray *sortedArray = [us sortResultsByUsername:self.searchResults searchTerm:searchTerm];
+                self.searchResults = [NSMutableArray arrayWithArray:sortedArray];
+                self.isSearching = YES;
+                [self.tableView reloadData];
+                [[TTUtility sharedInstance] internetConnectionFound];
+            }
+        }];
+    }
+}
+
+//- (void)keyboardWillShow:(NSNotification *)notification {
+//    //move the search field and collectionview up
+//    [self.view layoutIfNeeded];
+////    self.searchFieldTopConstraint.constant = 117;
+////    self.tableViewTopConstraint.constant = 137;
+////
+////    [UIView animateWithDuration:1.00
+////                     animations:^{
+////                         [self.view layoutIfNeeded];
+////                     }];
+//}
+//
+
+//-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+//    if(![event touchesForView:self.searchTextField]){
+//        [self dismissKeyboard];
+//    }
+//}
+//
+//-(void)touchesEnded:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+//    NSLog(@"touch ended");
+//}
+
+-(void)dismissKeyboard{
+    [self.searchTextField resignFirstResponder];
+    if([self.searchTextField.text isEqualToString:@""]){
+        [self.view layoutIfNeeded];
+    }
+}
+
+#pragma mark - UITextFieldDelegate
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string{
+    
+    NSString *typedText;
+    
+    if(range.location == textField.text.length)
+        typedText = [textField.text stringByAppendingString:string];
+    else typedText = [textField.text stringByReplacingCharactersInRange:range withString:string];
+    
+    [self filterResults:typedText];
+    
+    return YES;
 }
 
 @end
